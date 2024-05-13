7264dc367f47b0c179a4c137295b7f82ddb4a4ee
Valentyna_Olkhovska <valeskus13@gmail.com>
2024-05-13 17:22:16 +0300

    Initial commit

---
+++ screens/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import {View} from 'react-native';
+import {styles} from './styles';
+
+export function Home(): JSX.Element {
+  return <View style={styles.screenContainer} />;
+}
